1f90af9ab53e3245e855905a9aa5dd5618797c08
AnthonyG-Dev <giterugicheru@gmail.com>
2024-04-06 05:35:51 +0300

    review of the code solution

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ function returnsANamedFunction(named) {
   return function named() {};
 }
 
+// solution
+// function returnsANamedFunction() {
+//   return function named() {
+//     console.log("this is a named function");
+//   };
+// }
+
 function returnsAnAnonymousFunction() {
   return () => {};
 }
+
+// solution
+// function returnsAnAnonymousFunction() {
+//   return () => {console.log("This is an anonymous function")};
+// }
